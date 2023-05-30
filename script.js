819fc6ee2b08d6b1c2e582ec78fe8a5753898cc7
trukss <59446699+trukss@users.noreply.github.com>
2023-05-30 12:58:38 -0300

    Adiciona funcao playRound

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ function getComputerChoice() {
   } else if (aleatorio === 2) {
     return "Scissors"
   }
+}
+const playerSelection = ""
+const computerSelection = getComputerChoice()
 
-
-
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "Rock" && computerSelection == "Paper") {
+    return "Voce perde!, paper vence de Rock";
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return "Voce perde!, Scissors vence de paper";
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return "Voce perde!, Rock vence de Scissors";
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "Voce ganha!, Paper vence de Rock";
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "Voce ganha!, Scissors ganhar de Paper";
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "Voce ganha!, Rock ganha de Scisosrs"
+  } else {
+    return "E um empate";
+  }
 }

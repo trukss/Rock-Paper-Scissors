b19ab3eaffccabb5b8d37cd50076179ea455a2e0
trukss <59446699+trukss@users.noreply.github.com>
2023-05-30 22:37:59 -0300

    Adicina contador de pontos com defeito na function game

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let playerSelection;
 let computerSelection;
-
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
   let aleatorio = Math.floor(Math.random() * 3);
@@ -27,11 +28,11 @@ function playRound(playerSelection, computerSelection) {
   } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
     return "Voce ganha!, scissors ganhar de paper";
   } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-    return "Voce ganha!, rock ganha de scisosrs"
+    return "Voce ganha!, rock ganha de scisosrs";
   } else if (playerSelection == computerSelection) {
     return "É um empate";
   } else {
-    return "Valor invalido"
+    return "Valor invalido";
   }
 }
 
@@ -41,8 +42,24 @@ function game() {
     computerSelection = getComputerChoice();
     playerSelection = prompt("Escolha entre Rock, paper or scissors");
     console.log(playRound(playerSelection.toUpperCase(), computerSelection));
+    if (playRound(playerSelection, computerSelection) === "Voce perde!, paper vence de rock" || playRound(playerSelection, computerSelection) === "Voce perde!, scissors vence de paper" || "Voce perde!, rock vence de scissors") {
+      computerScore = computerScore++
+    } else if (playRound(playerSelection, computerSelection) === "Voce ganha!, paper vence de rock" || "Voce ganha!, scissors ganhar de paper" || "Voce ganha!, scissors ganhar de paper") {
+      playerScore = playerScore++
+    } else {
 
+    }
+    console.log(playerScore)
+    console.log(computerScore)
   }
+  if (playerScore > computerScore) {
+    console.log(`Parabens voce ganhou com ${playerScore} pontos`)
+  } else if (playerScore === computerScore) {
+    console.log("Infelismente houve um empate")
+  } else {
+    console.log(`Infelismente o computador ganhou com ${computerScore} pontos`)
+  }
+
 
 }
 
